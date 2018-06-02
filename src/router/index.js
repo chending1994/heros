5d6e2f2f5ccb94ce7d3f92e9fe6ba1f936d29ec5
chending <cdchenidng@126.com>
2018-06-02 10:47:08 +0800

    cdchending@126.com
    路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,18 @@ import Router from 'vue-router'
 
 // 加载路由规则对应的组件
 import HeroesList from '@/views/heroes/list';
+import WeaponsList from '@/views/weapons/list';
+import EquipsList from '@/views/equips/list';
+
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
     // { name: 'home', path: '/', redirect: '/heroes' },
-    { name: 'home', path: '/', redirect: { name: 'heroes'}},
-    { name: 'heroes', path: '/heroes', component: HeroesList }
+    { name: 'home', path: '/', redirect: { name: 'heroes'} },
+    { name: 'heroes', path: '/heroes', component: HeroesList },
+    { name: 'weapons', path: '/weapons', component: WeaponsList },
+    { name: 'equips', path: '/equips', component: EquipsList }
   ]
 })
